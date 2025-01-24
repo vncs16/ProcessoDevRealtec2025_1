d09136fd1c62d20b095910b4cf8a7772c0bf1270
Vinicius Aguiar <viniciusaguiarcp@gmail.com>
2025-01-24 17:15:32 -0300

    Simulador do timer, sendo cada segundo simulando um minuto para a cârama

--- teste_agro.py
+++ teste_agro.py
@@ -1,1 +1,16 @@
-print ("Hello World")
+#Bibliotecas
+import time
+
+#Variáveis
+temperatura = 0
+motor = False
+
+#Main
+
+while temperatura >= 0 and temperatura <= 15:
+    if motor == True:
+        temperatura -= 0.8
+    else:
+        temperatura += 1.5
+    print(temperatura)
+    time.sleep(1)
